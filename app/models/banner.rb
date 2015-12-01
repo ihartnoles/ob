70163770f97e2ddce9227ac5b1993060537ec5ae
Isaac Hartstein <ihartnoles@gmail.com>
2015-12-01 17:47:07 -0500

    fixed display of acceptance term in Verify Info Module

--- app/models/banner.rb
+++ app/models/banner.rb
@@ -8,11 +8,23 @@ class Banner < ActiveRecord::Base
 
 	#BEGIN: QUERIES TO BANINST1.AWS_ONBOARDING_MAIN
 		def self.find_student_by_netid(id)
-		 	get = connection.exec_query("select distinct Z_NUMBER, SARADAP_TERM_CODE_ENTRY, L_NAME, M_NAME, F_NAME, SGBSTDN_MAJR_CODE_1, SPRADDR_STREET_LINE1 ,SPRADDR_CITY ,SPRADDR_STAT_CODE, SPRADDR_ZIP, GOBTPAC_EXTERNAL_USER  FROM BANINST1.AWS_ONBOARDING_MAIN WHERE GOBTPAC_EXTERNAL_USER=#{connection.quote(id)} AND rownum = 1")
+		 	get = connection.exec_query("select distinct Z_NUMBER,  SUBSTR( SARADAP_TERM_CODE_ENTRY, 1 , 4 ) as year,
+									      CASE SUBSTR(SARADAP_TERM_CODE_ENTRY, 5 , 6 )
+									             WHEN '01' THEN 'Spring'
+									             WHEN '08' THEN 'Fall'
+									             WHEN '05' THEN 'Summer'
+									          ELSE ''
+									      END as term , L_NAME, M_NAME, F_NAME, SGBSTDN_MAJR_CODE_1, SPRADDR_STREET_LINE1 ,SPRADDR_CITY ,SPRADDR_STAT_CODE, SPRADDR_ZIP, GOBTPAC_EXTERNAL_USER  FROM BANINST1.AWS_ONBOARDING_MAIN WHERE GOBTPAC_EXTERNAL_USER=#{connection.quote(id)} AND rownum = 1")
 		end
 
 		def self.find_student_by_z(id)
-		 	get = connection.exec_query("select distinct Z_NUMBER, SARADAP_TERM_CODE_ENTRY, L_NAME, M_NAME, F_NAME, SGBSTDN_MAJR_CODE_1, SPRADDR_STREET_LINE1 ,SPRADDR_CITY ,SPRADDR_STAT_CODE, SPRADDR_ZIP, GOBTPAC_EXTERNAL_USER  FROM BANINST1.AWS_ONBOARDING_MAIN WHERE Z_NUMBER=#{connection.quote(id)} AND rownum = 1")
+		 	get = connection.exec_query("select distinct Z_NUMBER,  SUBSTR( SARADAP_TERM_CODE_ENTRY, 1 , 4 ) as year,
+									      CASE SUBSTR(SARADAP_TERM_CODE_ENTRY, 5 , 6 )
+									             WHEN '01' THEN 'Spring'
+									             WHEN '08' THEN 'Fall'
+									             WHEN '05' THEN 'Summer'
+									          ELSE ''
+									      END as term , L_NAME, M_NAME, F_NAME, SGBSTDN_MAJR_CODE_1, SPRADDR_STREET_LINE1 ,SPRADDR_CITY ,SPRADDR_STAT_CODE, SPRADDR_ZIP, GOBTPAC_EXTERNAL_USER  FROM BANINST1.AWS_ONBOARDING_MAIN WHERE Z_NUMBER=#{connection.quote(id)} AND rownum = 1")
 		end
 
 		def self.find_student_zip_by_z(id)
